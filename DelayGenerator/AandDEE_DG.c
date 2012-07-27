b4a640b2ec06d75aca964fcfeb1d77e5ed53eb8d
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2012-07-27 19:27:45 +0200

    add matrixi driving (using UpperRight TTL) and change frequency to 4Hz. modified:   AandDEE_DG.c: add matrixi TLL and LED; setup 4 Hz. modified:   Makefile: small change of /dev number. modified:   Makefile.template: small change of /dev number. new file:   VERSION: set version v0.0.1 (not coherent). modified:   _info.DG.txt: add run command lines for PCO cameras.

--- DelayGenerator/AandDEE_DG.c
+++ DelayGenerator/AandDEE_DG.c
@@ -46,11 +46,15 @@
 
 //TTL camera  (out)
 #define TTL_camera  TTL_UL
+//TTL matrixi  (out)
+#define TTL_matrixi TTL_UR
 
 //LED camera  (out)
 #define LED_camera  LED_UL
+//LED matrixi  (out)
+#define LED_matrixi LED_UR
 //LED wait next reset  (out)
-#define LED_wait  LED_BR
+#define LED_wait    LED_BR
 
 //! 
 /**
@@ -157,13 +161,18 @@ inline void record_image(const int delayUp,const int delay1)
     TTL_PORT|=_BV(TTL_camera);//TTL on camera
     ///LED
     LED_PORT|=_BV(LED_camera);//LED on camera
+    ///LED and TTL matrixi
+    LED_PORT|=_BV(LED_matrixi);//LED on matrixi
+    TTL_PORT|=_BV(TTL_matrixi);//TTL on matrixi
     //delay (i.e. TTL up time)
     _delay_ms(delayUp);//delay0
     //OFF
     TTL_PORT&=~_BV(TTL_camera);//TTL off camera
-    //delay (i.e. LED exposure time)
+    TTL_PORT&=~_BV(TTL_matrixi);//TTL off matrixi
+    ///delay (i.e. LED exposure time)
     _delay_ms(delay1);
     LED_PORT&=~_BV(LED_camera);//LED off camera
+    LED_PORT&=~_BV(LED_matrixi);//LED off matrixi
 }//record_image
 
 //
@@ -172,9 +181,9 @@ int main(void)
 //initialisation
 ///TTL
 //  TTL_DDR&=~_BV(TTL_UL);//TTL input
-  TTL_DDR|=_BV(TTL_camera);//TTL output: sync for camera (FlowMaster, ImagerIntense, Phantom, ...)
+  TTL_DDR|=_BV(TTL_camera)|_BV(TTL_matrixi);//TTL output: sync for camera (FlowMaster, ImagerIntense, Phantom, ...)
 ///LED
-  LED_DDR|=_BV(LED_camera)|_BV(LED_wait);//|_BV(LED_UL)|_BV(LED_UR);//LED output: TTL_UL and wait for reset
+  LED_DDR|=_BV(LED_camera)|_BV(LED_matrixi)|_BV(LED_wait);//|_BV(LED_UL);//LED output: TTL_UL and wait for reset
 
 /** /
 //mapping
@@ -192,17 +201,17 @@ int main(void)
 
 //internal delay generator
 ///TTL
-int period=1000;//period=delay0+delay1+delay2
+int period=250;//period=delay0+delay1+delay2
 int delayUp=10;//delay0
 int delayDown=period-delayUp;
 ///LED
-int exposure=250;
+int exposure=100;
 int delay1=exposure-delayUp;//exposure=delay0+delay1
 int delay2=delayDown-delay1;//delayDown=delay1+delay2
 
 //sequence
   int i;
-  for(i=0;i<10;++i) {record_image(delayUp,delay1);_delay_ms(delay2);}
+  for(i=0;i<100;++i) {record_image(delayUp,delay1);_delay_ms(delay2);}
   LED_PORT|=_BV(LED_wait);//LED on wait
 //loop
   while(1){}//infinite loop
